07efb134d3aa6282a246c882d7d9253e501fa439
Daniel Lopez <d@niel.to>
2026-05-15 16:07:30 -0700

    feat: add auth via clerk

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import TopNav from '#/components/top-nav'
+
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-    <main className='flex grow flex-col p-4'>
-      <div className='flex grow flex-col items-center justify-center space-y-4'>
-        <h1 className='font-bold'>nfour</h1>
-      </div>
-    </main>
+    <>
+      <TopNav />
+      <main className='flex grow flex-col p-4'>
+        <div className='flex grow flex-col items-center justify-center space-y-4'>
+          <p>welcome</p>
+        </div>
+      </main>
+    </>
   )
 }
