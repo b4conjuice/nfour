5679c9c33276eaebc3de5ddced0f8cb5fe8da157
Daniel Lopez <d@niel.to>
2026-05-15 18:30:41 -0700

    chore(trpc/router): remove todos router

--- src/integrations/trpc/router.ts
+++ src/integrations/trpc/router.ts
@@ -1,26 +1,6 @@
-import { z } from 'zod'
+import { createTRPCRouter } from './init'
 
-import { createTRPCRouter, publicProcedure } from './init'
-
-import type { TRPCRouterRecord } from '@trpc/server'
 import { notesRouter } from '@/integrations/trpc/routers/notes'
-
-const todos = [
-  { id: 1, name: 'Get groceries' },
-  { id: 2, name: 'Buy a new phone' },
-  { id: 3, name: 'Finish the project' },
-]
-
-const todosRouter = {
-  list: publicProcedure.query(() => todos),
-  add: publicProcedure
-    .input(z.object({ name: z.string() }))
-    .mutation(({ input }) => {
-      const newTodo = { id: todos.length + 1, name: input.name }
-      todos.push(newTodo)
-      return newTodo
-    }),
-} satisfies TRPCRouterRecord
 
 export const trpcRouter = createTRPCRouter({
   notes: notesRouter,
