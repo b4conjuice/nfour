80b3c1b558ddb60a61d856ee5c5895d916858fe7
Daniel Lopez <d@niel.to>
2026-05-15 21:50:40 -0700

    style(note-list): add spacing and border to note list items

--- src/components/note-list.tsx
+++ src/components/note-list.tsx
@@ -4,15 +4,15 @@ import type { Note } from '@/lib/types'
 
 export default function NoteList({ notes }: { notes: Note[] }) {
   return (
-    <ul className='flex flex-col gap-4'>
+    <ul className='divide-cb-dusty-blue divide-y'>
       {notes.map(note => (
-        <li key={note.id}>
+        <li key={note.id} className='group flex gap-2'>
           <Link
             to='/notes/$noteId'
             params={{
               noteId: String(note.id),
             }}
-            className='text-cb-pink hover:text-cb-pink/75'
+            className='text-cb-pink hover:text-cb-pink/75 flex grow items-center justify-between py-4 group-first:pt-0'
           >
             {note.title}
           </Link>
