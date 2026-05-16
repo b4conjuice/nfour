ef1bc40da065531bdd330cfc3d79b1cfc0a75c83
Daniel Lopez <d@niel.to>
2026-05-16 08:21:50 -0700

    feat(note-list): add search

--- src/components/note-list.tsx
+++ src/components/note-list.tsx
@@ -1,26 +1,54 @@
 import { Link } from '@tanstack/react-router'
 
 import type { Note } from '@/lib/types'
+import useSearch from '@/lib/useSearch'
 
 export default function NoteList({ notes }: { notes: Note[] }) {
+  const { search, setSearch, results, searchRef } = useSearch({
+    // initialSearch: query ? String(query) : '',
+    // list: taggedNotes || [],
+    initialSearch: '',
+    list: notes,
+    options: {
+      keys: ['title', 'body'],
+    },
+  })
   return (
-    <ul className='divide-cb-dusty-blue divide-y'>
-      {notes.map(note => (
-        <li key={note.id} className='group flex gap-2'>
-          <Link
-            to='/notes/$noteId'
-            params={{
-              noteId: String(note.id),
-            }}
-            className='text-cb-pink hover:text-cb-pink/75 flex grow items-center justify-between py-4 group-first:pt-0'
-          >
-            <div>
-              <div>{note.title}</div>
-              {note.tags.length > 0 && <div>{note.tags.join(' ')}</div>}
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className='flex'>
+        <input
+          ref={searchRef}
+          type='text'
+          className='bg-cb-blue w-full disabled:pointer-events-none disabled:opacity-25'
+          placeholder='search'
+          value={search}
+          onChange={e => {
+            const { value } = e.target
+            setSearch(value)
+            // const url = `${pathname}${value ? `?q=${value}` : ''}`
+            // router.push(url)
+          }}
+          disabled={!(notes.length && notes.length > 0)}
+        />
+      </div>
+      <ul className='divide-cb-dusty-blue divide-y'>
+        {results.map(note => (
+          <li key={note.id} className='group flex gap-2'>
+            <Link
+              to='/notes/$noteId'
+              params={{
+                noteId: String(note.id),
+              }}
+              className='text-cb-pink hover:text-cb-pink/75 flex grow items-center justify-between py-4 group-first:pt-0'
+            >
+              <div>
+                <div>{note.title}</div>
+                {note.tags.length > 0 && <div>{note.tags.join(' ')}</div>}
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
